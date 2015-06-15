c628684cd7c5804032b9080b7feac8d5d4a3c060
Shin Tanimoto <cel@pos.to>
2015-06-16 02:22:20 +0900

    moa-sample-4

--- moa-sample-4/student-client-4/src/main/java/application/client/StudentClient.java
+++ moa-sample-4/student-client-4/src/main/java/application/client/StudentClient.java
@@ -2,9 +2,7 @@ package application.client;
 
 import domain.Student;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
 import service.StudentService;
 
 @Component
